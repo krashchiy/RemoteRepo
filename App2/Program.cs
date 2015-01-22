95ff0b2bb82d2b02686433f208c6e4b8e0b51bf9
krashchiy <creativity3@yandex.ru>
2015-01-22 10:28:04 +0500

    New solution created

---
+++ App2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bro = "borrow";
+            Console.Write(bro); 
+        }
+
+        static bool word(string w)
+        {
+            return w == "borrow";
+        }
+    }
+}
